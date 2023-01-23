ff4b0048eceb632dd152fd40cacc48b7b9cfa21f
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-01-23 18:32:46 +0100

    chore(babel): set bugfixes to true

--- packages/exception/babel.config.cjs
+++ packages/exception/babel.config.cjs
@@ -1,19 +1,39 @@
-module.exports = {
-  comments: false,
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
-  plugins: [
-    '@babel/plugin-transform-spread',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-classes',
-  ],
-  assumptions: {
-    // for class transform
-    constantSuper: true, // @link https://babeljs.io/docs/en/assumptions#constantsuper
-    noClassCalls: true,
-    setClassMethods: true, // @link https://babeljs.io/docs/en/assumptions#setclassmethods
-    superIsCallableConstructor: true,
-    setPublicClassFields: true,
-    // for spread
-    iterableIsArray: true, // @link https://babeljs.io/docs/en/assumptions#iterableisarray
-  },
+// @ts-check
+
+/** @type {import('@babel/core').ConfigFunction} */
+module.exports = (api) => {
+  api.cache(false);
+
+  return {
+    comments: false,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          // https://babeljs.io/docs/en/babel-preset-env.html#bugfixes
+          bugfixes: true,
+          exclude: [
+            // '@babel/plugin-transform-regenerator',
+            // '@babel/plugin-transform-parameters',
+          ],
+        },
+      ],
+      '@babel/preset-typescript',
+    ],
+    plugins: [
+      '@babel/plugin-transform-spread',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-transform-classes',
+    ],
+    assumptions: {
+      // for class transform
+      constantSuper: true, // @link https://babeljs.io/docs/en/assumptions#constantsuper
+      noClassCalls: true,
+      setClassMethods: true, // @link https://babeljs.io/docs/en/assumptions#setclassmethods
+      superIsCallableConstructor: true,
+      setPublicClassFields: true,
+      // for spread
+      iterableIsArray: true, // @link https://babeljs.io/docs/en/assumptions#iterableisarray
+    },
+  };
 };
