79eb6ced87906b5f535a9d1dbd4d4d461531813b
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-06-14 20:26:10 +0200

    feat: add field validation errors to 422

--- packages/exception/src/client/HttpUnprocessableEntity.ts
+++ packages/exception/src/client/HttpUnprocessableEntity.ts
@@ -1,9 +1,14 @@
 import { HttpClientException } from '../base';
-import type { HttpExceptionParams } from '../types';
+import type { HttpExceptionParams, ValidationError } from '../types';
 import { getSuper } from '../utils';
 
 /**
- * 422 Unprocessable entity (client / webdav specific)
+ * 422 Unprocessable entity (client / webdav specific per RFC / used for validation errors in most apis)
+ *
+ * Be aware that a lot of apis/frameworks will use 422 Unprocessable Entity to indicate (form field) validation errors
+ * when posting data (rails, github, api-platform...). See also 400 for simple request errors.
+ *
+ * @see https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#client-errors
  *
  * The server understands the content type of the request entity (hence a 415 Unsupported Media Type status code
  * is inappropriate), and the syntax of the request entity is correct (thus a 400 Bad Request status code is
@@ -17,9 +22,18 @@ import { getSuper } from '../utils';
  */
 export class HttpUnprocessableEntity extends HttpClientException {
   static readonly STATUS = 422;
-  constructor(msgOrParams?: HttpExceptionParams | string) {
+  public readonly errors: ValidationError[];
+  constructor(
+    msgOrParams?:
+      | (HttpExceptionParams & { errors?: ValidationError[] })
+      | string
+  ) {
     const name = 'UnprocessableEntity';
     super(422, getSuper(name, msgOrParams));
+    this.errors =
+      typeof msgOrParams === 'object' && msgOrParams.errors
+        ? msgOrParams.errors
+        : [];
     Object.setPrototypeOf(this, HttpUnprocessableEntity.prototype);
     this.name = `Http${name}`;
   }
