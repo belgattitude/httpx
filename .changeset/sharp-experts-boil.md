42cdf637c3d95b85f29e003016f33ac5c554667a
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-06-14 23:25:42 +0200

    perf: extracting regexp, remove extraneous parenthesis

---
+++ .changeset/sharp-experts-boil.md
@@ -0,0 +1,5 @@
+---
+'@httpx/exception': patch
+---
+
+Minor perf by extracting regexp into an hoisted var
--- packages/exception/src/utils/getMsgFromCls.ts
+++ packages/exception/src/utils/getMsgFromCls.ts
@@ -1,10 +1,11 @@
+const splitCapsRegexp = /[A-Z]/g;
 /**
  * Return default message based on http exception className
  * @internal
  */
 export const getMsgFromCls = (className: string) => {
   return className
-    .replace(/[A-Z]/g, (match) => ` ${match}`)
+    .replace(splitCapsRegexp, (match) => ` ${match}`)
     .trim()
     .split(' ')
     .map((word, idx) => (idx === 0 ? word : word.toLowerCase()))
