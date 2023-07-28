c966acb0b07329b3bf8a30fa1f84b39f175872c5
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-07-28 12:46:51 +0200

    Ensure modern browser support (#500)
    * docs: fix node min version
    
    * feat(dsn-parser): ensure modern browser support

--- packages/dsn-parser/.size-limit.cjs
+++ packages/dsn-parser/.size-limit.cjs
@@ -2,12 +2,11 @@ module.exports = [
   {
     name: 'JS (ESM)',
     path: ['dist/index.mjs'],
-    limit: '1.20KB',
+    limit: '1.15KB',
   },
   {
     name: 'JS (CJS)',
     path: ['dist/index.cjs'],
-    limit: '1.20KB',
+    limit: '1.43KB',
   },
-
 ];
