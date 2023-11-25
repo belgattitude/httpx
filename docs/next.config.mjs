d43caf843a1d46eb5b017a626caea1de182e9501
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-11-25 19:13:05 +0100

    ci: ci-deploy-docs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,11 +5,23 @@ const withNextra = nextra({
   themeConfig: './theme.config.tsx',
 });
 
+/** Useful when you publish the static export in a different basePath */
+const basePath = process.env.NEXT_BUILD_ENV_BASE_PATH ?? undefined;
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  ...(basePath ? { basePath } : {}),
   output: 'export',
+  productionBrowserSourceMaps:
+    process.env.NEXT_BUILD_ENV_SOURCEMAPS !== 'false',
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_BUILD_ENV_LINT === 'false',
+  },
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_BUILD_ENV_TYPECHECK === 'false',
+  },
   images: {
     unoptimized: true,
   },
