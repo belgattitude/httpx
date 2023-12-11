8b66bf467fe588d231a4cde888022d05c22d0ff1
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-12-11 23:57:30 +0100

    test: improve tests for isPlainObject

--- packages/json-api/src/__tests__/typeguards.test.ts
+++ packages/json-api/src/__tests__/typeguards.test.ts
@@ -7,9 +7,15 @@ describe('Typeguards tests', () => {
       [{ name: 'seb' }, true],
       [{ children: [{ test: 1 }], name: 'deep' }, true],
       [new Date(), false],
+      [new (class Cls {})(), false],
+      [new Map(), false],
+      [new Error(), false],
+      [new Set(), false],
       [false, false],
       [undefined, false],
       [null, false],
+      [Number.NaN, false],
+      [new Request('http://localhost'), false],
       [() => 'cool', false],
     ])('when "%p" is given, should return %p', (v, expected) => {
       expect(isPlainObject(v)).toStrictEqual(expected);
