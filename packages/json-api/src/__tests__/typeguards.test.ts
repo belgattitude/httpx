4098c29e43bc02369041bf887c8df6fa41211a79
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-12-12 00:02:14 +0100

    test: add more tests

--- packages/json-api/src/__tests__/typeguards.test.ts
+++ packages/json-api/src/__tests__/typeguards.test.ts
@@ -7,20 +7,23 @@ describe('Typeguards tests', () => {
       [{ name: 'seb' }, true],
       [{ children: [{ test: 1 }], name: 'deep' }, true],
       [{ constructor: { name: 'Object2' } }, true],
-      [new Date(), false],
+      // False
+      [() => 'cool', false],
       [new (class Cls {})(), false],
       [new (class extends Object {})(), false],
+      [new Date(), false],
       [new Map(), false],
       [new Error(), false],
       [new Set(), false],
+      [new Request('http://localhost'), false],
       [/(\d+)/, false],
       [new Promise(() => {}), false],
+      ['hello', false],
       [false, false],
       [undefined, false],
       [null, false],
+      [10, false],
       [Number.NaN, false],
-      [new Request('http://localhost'), false],
-      [() => 'cool', false],
     ])('when "%p" is given, should return %p', (v, expected) => {
       expect(isPlainObject(v)).toStrictEqual(expected);
     });
