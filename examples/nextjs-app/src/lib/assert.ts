b5411f086034669630c21c34da1b839a6e134120
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-01-01 19:48:52 +0100

    feat: add test for type exports

--- examples/nextjs-app/src/lib/assert.ts
+++ examples/nextjs-app/src/lib/assert.ts
@@ -1,5 +1,12 @@
-import { isEan13 } from '@httpx/assert';
+import { isEan13, isUuidV4, type UuidV4 } from '@httpx/assert';
 
 export const checkEan13 = (v: unknown): boolean => {
   return isEan13(v);
+};
+
+export const testTypeExports = (v: unknown): UuidV4 | null => {
+  if (isUuidV4(v)) {
+    return v;
+  }
+  return null;
 };
