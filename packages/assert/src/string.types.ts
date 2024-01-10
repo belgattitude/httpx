21be0fa7fa691bae059a28741f0ff2bdf913c209
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-01-10 22:07:37 +0100

    fix: weak opaque type signature for StringNonEmpty

--- packages/assert/src/string.types.ts
+++ packages/assert/src/string.types.ts
@@ -1,7 +1,6 @@
 import type { WeakOpaqueContainer } from './types/opaque.types';
 
-export type StringNonEmpty = string &
-  WeakOpaqueContainer<'ParsableStrictIsoDateZ'>;
+export type StringNonEmpty = string & WeakOpaqueContainer<'StringNonEmpty'>;
 export type ParsableSafeInt = string & WeakOpaqueContainer<'ParsableSafeInt'>;
 export type ParsableStrictIsoDateZ = string &
   WeakOpaqueContainer<'ParsableStrictIsoDateZ'>;
