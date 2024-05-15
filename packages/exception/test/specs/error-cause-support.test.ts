c897aeb965a3eabc3f71aa9729caf8c02913c636
Sebastien Vanvelthem <belgattitude@gmail.com>
2024-05-15 19:22:12 +0200

    chore: update vitest beta

--- packages/exception/test/specs/error-cause-support.test.ts
+++ packages/exception/test/specs/error-cause-support.test.ts
@@ -6,7 +6,6 @@ import {
   HttpServerException,
 } from '../../src/base';
 import { HttpNotFound } from '../../src/client';
-import { supportsErrorCause } from '../../src/support/supportsErrorCause';
 
 describe(`when Error.cause isn't supported`, () => {
   beforeEach(() => {
@@ -16,18 +15,6 @@ describe(`when Error.cause isn't supported`, () => {
     // vi.restoreAllMocks();
     // vi.resetModules();
   });
-
-  const cause = new Error('cause');
-  const params = {
-    cause,
-  };
-
-  const scenarios: [name: string, cls: HttpException][] = [
-    ['HttpException', new HttpException(500, params)],
-    ['HttpClientException', new HttpClientException(404, params)],
-    ['HttpServerException', new HttpServerException(500, params)],
-    ['HttpNotFound', new HttpNotFound(params)],
-  ];
 
   vi.mock(
     import('../../src/support/supportsErrorCause'),
@@ -41,8 +28,19 @@ describe(`when Error.cause isn't supported`, () => {
     }
   );
 
+  const cause = new Error('cause');
+  const params = {
+    cause,
+  };
+
+  const scenarios: [name: string, cls: HttpException][] = [
+    ['HttpException', new HttpException(500, params)],
+    ['HttpClientException', new HttpClientException(404, params)],
+    ['HttpServerException', new HttpServerException(500, params)],
+    ['HttpNotFound', new HttpNotFound(params)],
+  ];
+
   it.each(scenarios)('should ignore the cause for %s.', (_name, err) => {
-    expect(supportsErrorCause()).toStrictEqual(false);
     expect(err.cause).toStrictEqual(undefined);
   });
 });
