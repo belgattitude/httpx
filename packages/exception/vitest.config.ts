04c4d3b5a01434809ab8cde5bbc6a484b8f8ebbe
Sebastien Vanvelthem <belgattitude@gmail.com>
2024-05-15 19:56:14 +0200

    chore: update vitest beta

--- packages/exception/vitest.config.ts
+++ packages/exception/vitest.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
     deps: {
       optimizer: {
         web: {
-          enabled: true,
+          enabled: false,
         },
-        ssr: { enabled: true },
+        ssr: { enabled: false },
       },
     },
     typecheck: {
@@ -57,7 +57,7 @@ export default defineConfig({
     // By default, vitest does not provide global APIs for explicitness. If you prefer to use the APIs globally like Jest,
     // you can pass the --globals option to CLI or add globals: true in the config.
     // https://vitest.dev/config/#globals
-    globals: true,
+    globals: false,
     include: testFiles,
     // To mimic Jest behaviour regarding mocks.
     mockReset: true,
