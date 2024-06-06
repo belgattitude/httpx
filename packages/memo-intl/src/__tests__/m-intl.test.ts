91b182d32d9c846e2f945c4142ce6527c49e5972
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-06-07 00:14:44 +0200

    feat(memo-intl): expose cache methods

--- packages/memo-intl/src/__tests__/m-intl.test.ts
+++ packages/memo-intl/src/__tests__/m-intl.test.ts
@@ -3,26 +3,43 @@ import { expectTypeOf } from 'vitest';
 import { MIntl } from '../index';
 
 describe('MIntl tests', () => {
-  it('should return a Intl.NumberFormatter', () => {
-    const formatter = MIntl.NumberFormat('fr-FR', {
-      style: 'currency',
-      currency: 'EUR',
-      notation: 'compact',
-      minimumFractionDigits: 2,
+  describe('Intl methods', () => {
+    it('should return a Intl.NumberFormatter', () => {
+      const formatter = MIntl.NumberFormat('fr-FR', {
+        style: 'currency',
+        currency: 'EUR',
+        notation: 'compact',
+        minimumFractionDigits: 2,
+      });
+      expect(formatter.format(10.1345)).toBe('10,13 €');
+      expectTypeOf(formatter).toEqualTypeOf<Intl.NumberFormat>();
     });
-    expect(formatter.format(10.1345)).toBe('10,13 €');
-    expectTypeOf(formatter).toEqualTypeOf<Intl.NumberFormat>();
+    it('should return a Intl.DateTimeFormatter', () => {
+      const formatter = MIntl.DateTimeFormat('fr-FR', {
+        dateStyle: 'full',
+        timeStyle: 'full',
+        timeZone: 'UTC',
+      });
+      const date = Date.parse('2024-05-29T07:42:43.230Z');
+      expect(formatter.format(date)).toBe(
+        'mercredi 29 mai 2024 à 07:42:43 temps universel coordonné'
+      );
+      expectTypeOf(formatter).toEqualTypeOf<Intl.DateTimeFormat>();
+    });
   });
-  it('should return a Intl.DateTimeFormatter', () => {
-    const formatter = MIntl.DateTimeFormat('fr-FR', {
-      dateStyle: 'full',
-      timeStyle: 'full',
-      timeZone: 'UTC',
+  describe('Cache access', () => {
+    it('should return a Intl.NumberFormatter', () => {
+      MIntl.cache.clear();
+      const _nf1 = MIntl.NumberFormat('fr-FR');
+      const _nf2 = MIntl.NumberFormat('fr-FR');
+      const _df2 = MIntl.DateTimeFormat('fr-FR');
+      const _df3 = MIntl.DateTimeFormat('fr-FR');
+      const _df4 = MIntl.DateTimeFormat('fr-FR', {
+        dateStyle: 'full',
+      });
+      expect(MIntl.cache.stats()).toStrictEqual({ cachedInstances: 3 });
+      MIntl.cache.clear();
+      expect(MIntl.cache.stats()).toStrictEqual({ cachedInstances: 0 });
     });
-    const date = Date.parse('2024-05-29T07:42:43.230Z');
-    expect(formatter.format(date)).toBe(
-      'mercredi 29 mai 2024 à 07:42:43 temps universel coordonné'
-    );
-    expectTypeOf(formatter).toEqualTypeOf<Intl.DateTimeFormat>();
   });
 });
