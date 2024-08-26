f31c8ea06708498cf8cf9f37e336ebde7118b52f
Sebastien Vanvelthem <belgattitude@gmail.com>
2024-08-26 15:06:55 +0200

    feat: fixes treeu

--- packages/treeu/src/tree.ts
+++ packages/treeu/src/tree.ts
@@ -1,15 +1,10 @@
-import { TreeSearch } from './search/tree-search';
 import type { TreeNode, TreeNodeValue } from './tree.types';
 
 export class Tree<
   TValue extends TreeNodeValue | undefined,
   TKey extends string = string,
 > {
-  public readonly search: TreeSearch<TValue, TKey>;
-
-  constructor(protected readonly treeNodes: TreeNode<TValue, TKey>[]) {
-    this.search = new TreeSearch<TValue, TKey>(this.treeNodes);
-  }
+  constructor(protected readonly treeNodes: TreeNode<TValue, TKey>[]) {}
 
   getTreeNodes = (): TreeNode<TValue, TKey>[] => {
     return this.treeNodes;
