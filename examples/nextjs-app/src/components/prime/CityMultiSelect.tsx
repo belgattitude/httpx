00a1c011caea049dcc9e53a0f17faf11b1b63124
Sebastien Vanvelthem <belgattitude@gmail.com>
2024-09-05 08:24:02 +0200

    feat: updated examples

--- examples/nextjs-app/src/components/prime/CityMultiSelect.tsx
+++ examples/nextjs-app/src/components/prime/CityMultiSelect.tsx
@@ -32,7 +32,7 @@ export const CityMultiSelect: FC<Props> = (props) => {
         display="chip"
         placeholder="Select Cities"
         maxSelectedLabels={3}
-        className="w-full md:w-20rem"
+        className="w-full"
       />
     </div>
   );
