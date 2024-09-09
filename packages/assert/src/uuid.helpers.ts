db250a04bbe07720d6484d0bec6305e6d64fe53e
Sebastien Vanvelthem <belgattitude@gmail.com>
2024-09-09 14:10:07 +0200

    refactor!: getUuidVersion now returns null instead of false

--- packages/assert/src/uuid.helpers.ts
+++ packages/assert/src/uuid.helpers.ts
@@ -4,13 +4,13 @@ import { uuidSupportedVersions } from './uuid.utils';
 /**
  * Adapted from https://github.com/uuidjs/uuid/blob/main/src/version.js
  */
-export const getUuidVersion = (uuid: string): UuidVersion | false => {
+export const getUuidVersion = (uuid: string): UuidVersion | null => {
   if (typeof uuid !== 'string') {
-    return false;
+    return null;
   }
   const v = Number.parseInt(uuid.slice(14, 15), 16);
   if (uuidSupportedVersions.has(v)) {
     return v as UuidVersion;
   }
-  return false;
+  return null;
 };
