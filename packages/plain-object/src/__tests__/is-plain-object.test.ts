ea082089ed3d3efc182fb3e125c0698e647216e4
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-10-12 21:16:11 +0200

    test: run tests con cloudflare workers

--- packages/plain-object/src/__tests__/is-plain-object.test.ts
+++ packages/plain-object/src/__tests__/is-plain-object.test.ts
@@ -4,6 +4,7 @@ import { isPlainObject as reduxIsPlainObject } from 'redux';
 import { isPlainObject } from '../is-plain-object';
 
 const isNodeLike = !('window' in globalThis);
+const isCloudflareWorker = 'caches' in globalThis;
 
 describe('isPlainObject', () => {
   const str = 'key';
@@ -32,7 +33,6 @@ describe('isPlainObject', () => {
     [JSON.parse('{}'), true],
     [new Proxy({}, {}), true],
     [new Proxy({ key: 'proxied_key' }, {}), true],
-
     [
       {
         [Symbol.iterator]: function* () {
@@ -41,7 +41,6 @@ describe('isPlainObject', () => {
       },
       true,
     ],
-
     [
       {
         [Symbol.toStringTag]: 'string-tagged',
@@ -74,8 +73,6 @@ describe('isPlainObject', () => {
     [fnWithProto, false],
     // Symbols
     [Symbol('cool'), false],
-    // globalThis
-    [globalThis, false],
     // Built-in classes
     [new Date(), false],
     [new Map(), false],
@@ -122,80 +119,89 @@ describe('isPlainObject', () => {
     }
   );
 
-  describe('Compatibility with other libraries', () => {
-    describe.skip('Compatibility with is-plain-obj', () => {
-      it.each(cases)(
-        'compat when "%s" is given, should return %s',
-        (v, _expected) => {
-          expect(isPlainObject(v)).toBe(isPlainObj(v));
-        }
-      );
-    });
-
-    // eslint-disable-next-line jest/valid-describe-callback
-    describe.skip('Compatibility with es-toolkit/is-plain-object', async () => {
-      const esToolkitIsPlainObject = await import('es-toolkit').then(
-        (mod) => mod.isPlainObject
-      );
-      it.each(cases)(
-        'compat when "%s" is given, should return %s',
-        (v, _expected) => {
-          expect(isPlainObject(v)).toBe(esToolkitIsPlainObject(v as object));
-        }
-      );
-    });
-
-    // eslint-disable-next-line jest/valid-describe-callback
-    describe('Compatibility with redux/isPlainObject', async () => {
-      it.each(cases)(
-        'compat when "%s" is given, should return %s',
-        (v, _expected) => {
-          expect(reduxIsPlainObject(v)).toBe(isPlainObject(v));
-        }
-      );
-      it.skipIf(!isNodeLike)(
-        'should support cross-realms - node:runInNewContext',
-        async () => {
-          // eslint-disable-next-line import-x/no-nodejs-modules
-          const runInNewContext = await import('node:vm').then(
-            (mod) => mod.runInNewContext
-          );
-          expect(reduxIsPlainObject(runInNewContext('({})'))).toBe(true);
-          const sandbox = { fromAnotherRealm: false };
-          runInNewContext('fromAnotherRealm = {}', sandbox);
-          expect(isPlainObject(sandbox.fromAnotherRealm)).toBe(true);
-        }
-      );
-    });
-
-    // eslint-disable-next-line jest/valid-describe-callback
-    describe.skip('Compatibility with jonschlinkert/is-plain-object', async () => {
-      // @ts-expect-error packaging of this lib is not compatible with latest ts / module
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const jonschlinkertIsPlainObject = await import('is-plain-object').then(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
-        (mod) => mod.isPlainObject
-      );
-      it.each(cases)(
-        'compat when "%s" is given, should return %s',
-        (v, _expected) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-          expect(isPlainObject(v)).toBe(jonschlinkertIsPlainObject(v));
-        }
-      );
-    });
+  it('should return true when globalThis is given', () => {
+    expect(isPlainObject(globalThis)).toBe(false);
   });
+
+  describe.skipIf(isCloudflareWorker)(
+    'Compatibility with other libraries',
+    () => {
+      describe.skip('Compatibility with is-plain-obj', () => {
+        it.each(cases)(
+          'compat when "%s" is given, should return %s',
+          (v, _expected) => {
+            expect(isPlainObject(v)).toBe(isPlainObj(v));
+          }
+        );
+      });
+
+      // eslint-disable-next-line jest/valid-describe-callback
+      describe.skip('Compatibility with es-toolkit/is-plain-object', async () => {
+        const esToolkitIsPlainObject = await import('es-toolkit').then(
+          (mod) => mod.isPlainObject
+        );
+        it.each(cases)(
+          'compat when "%s" is given, should return %s',
+          (v, _expected) => {
+            expect(isPlainObject(v)).toBe(esToolkitIsPlainObject(v as object));
+          }
+        );
+      });
+
+      // eslint-disable-next-line jest/valid-describe-callback
+      describe('Compatibility with redux/isPlainObject', async () => {
+        it.each(cases)(
+          'compat when "%s" is given, should return %s',
+          (v, _expected) => {
+            expect(reduxIsPlainObject(v)).toBe(isPlainObject(v));
+          }
+        );
+        it.skipIf(!isNodeLike)(
+          'should support cross-realms - node:runInNewContext',
+          async () => {
+            // eslint-disable-next-line import-x/no-nodejs-modules
+            const runInNewContext = await import('node:vm').then(
+              (mod) => mod.runInNewContext
+            );
+            expect(reduxIsPlainObject(runInNewContext('({})'))).toBe(true);
+            const sandbox = { fromAnotherRealm: false };
+            runInNewContext('fromAnotherRealm = {}', sandbox);
+            expect(isPlainObject(sandbox.fromAnotherRealm)).toBe(true);
+          }
+        );
+      });
+
+      // eslint-disable-next-line jest/valid-describe-callback
+      describe.skip('Compatibility with jonschlinkert/is-plain-object', async () => {
+        // @ts-expect-error packaging of this lib is not compatible with latest ts / module
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const jonschlinkertIsPlainObject = await import('is-plain-object').then(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
+          (mod) => mod.isPlainObject
+        );
+        it.each(cases)(
+          'compat when "%s" is given, should return %s',
+          (v, _expected) => {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+            expect(isPlainObject(v)).toBe(jonschlinkertIsPlainObject(v));
+          }
+        );
+      });
+    }
+  );
 });
 
-describe('Support node:vm.runInNewContext', () => {
-  it.skipIf(!isNodeLike)('should support vm', async () => {
-    // eslint-disable-next-line import-x/no-nodejs-modules
-    const runInNewContext = await import('node:vm').then(
-      (mod) => mod.runInNewContext
-    );
-    expect(isPlainObject(runInNewContext('({})'))).toBe(true);
-    expect(isPlainObject(runInNewContext('(false)'))).toBe(false);
-    expect(isPlainObject(runInNewContext('(new Date())'))).toBe(false);
-    expect(reduxIsPlainObject(runInNewContext('({})'))).toBe(true);
-  });
-});
+describe.skipIf(!isNodeLike || isCloudflareWorker)(
+  'Support node:vm.runInNewContext',
+  () => {
+    it('should support vm', async () => {
+      // eslint-disable-next-line import-x/no-nodejs-modules
+      const runInNewContext = await import('node:vm').then(
+        (mod) => mod.runInNewContext
+      );
+      expect(isPlainObject(runInNewContext('({})'))).toBe(true);
+      expect(isPlainObject(runInNewContext('(false)'))).toBe(false);
+      expect(isPlainObject(runInNewContext('(new Date())'))).toBe(false);
+    });
+  }
+);
