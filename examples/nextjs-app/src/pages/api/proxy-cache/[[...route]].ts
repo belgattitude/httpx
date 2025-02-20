5a1e704545d54b92f10b1505ac6d1eedb0aa5423
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-02-20 13:57:44 +0100

    feat: initial mcache

--- examples/nextjs-app/src/pages/api/proxy-cache/[[...route]].ts
+++ examples/nextjs-app/src/pages/api/proxy-cache/[[...route]].ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 import type { PageConfig } from 'next';
 
 import { proxyCacheConfig } from '@/server/config/proxy-cache.config';
-import { ProxyCache, type ProxyCacheItem } from '@/server/lib/proxy-cache';
+import { ProxyCache } from '@/server/lib/proxy-cache';
 
 export const config: PageConfig = {
   runtime: 'nodejs',
@@ -17,15 +17,8 @@ const proxyCacheBasePath = proxyCacheConfig.proxy.basePath;
 
 const app = new Hono().basePath(proxyCacheBasePath);
 
-type CacheKeyParams = ProxyCacheItem['requestParams'] & {
-  session: {
-    userId: string | null;
-  } | null;
-};
-
 app.all('*', async (c) => {
   return proxyCache.getResponse(c);
 });
 
-export type HonoProxyCacheRouter = typeof app;
 export default handle(app);
