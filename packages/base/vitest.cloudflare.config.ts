25b2735434f822b1dd7591982c601bd10687b8e0
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-04-11 12:57:39 +0200

    feat: init base64 package

---
+++ packages/base/vitest.cloudflare.config.ts
@@ -0,0 +1,35 @@
+import { defineWorkersConfig } from '@cloudflare/vitest-pool-workers/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const testFiles = ['./src/**/*.test.{js,ts}', './test/**/*.test.{js,ts}'];
+
+export default defineWorkersConfig({
+  esbuild: {
+    target: ['node18'],
+  },
+  plugins: [tsconfigPaths()],
+  cacheDir: '../../.cache/vite/httpx-base',
+  test: {
+    // @link https://vitest.dev/config/#clearmocks
+    clearMocks: true,
+    poolOptions: {
+      workers: {
+        wrangler: {
+          configPath: '../../devtools/vitest/wrangler.toml',
+        },
+      },
+    },
+    exclude: [
+      '**/node_modules/**',
+      'dist/**',
+      '**/coverage/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
+    globals: true,
+    include: testFiles,
+    // To mimic Jest behaviour regarding mocks.
+    mockReset: true,
+    passWithNoTests: true,
+    restoreMocks: true,
+  },
+});
