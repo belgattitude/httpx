089dfb5e58d345d4a82a36f32bc50268882dc2ec
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-07-30 10:32:34 +0200

    feat: next version

--- packages/xcache/src/x-cache.types.ts
+++ packages/xcache/src/x-cache.types.ts
@@ -9,37 +9,6 @@ export type CacheableAsyncFunction = (
   ...args: any[]
 ) => Promise<CacheableValues>;
 
-export type XCacheRunAsyncResult<TFunction extends CacheableAsyncFunction> = {
-  data: Awaited<ReturnType<TFunction>>;
-  meta: {
-    cached: boolean;
-    cacheKey: string;
-  };
-};
-
-type KeyTuple<TFunction> = [
-  string,
-  ...(TFunction extends (...args: infer P) => unknown ? P : never),
-];
-
-type TestFnParams = {
-  name: string;
-};
-const fetchDataFn = async (params: TestFnParams) => {
-  return {
-    message: `Hello ${params.name}`,
-  };
-};
-
-export interface XCacheRunAsyncParamsBackup<
-  TFunction extends CacheableAsyncFunction,
-> {
-  key: CacheKeyTuple;
-  fn: TFunction;
-  namespace?: string;
-  ttl?: number;
-}
-
 export interface XCacheRunAsyncParams<
   TFunction extends CacheableAsyncFunction,
 > {
@@ -48,40 +17,3 @@ export interface XCacheRunAsyncParams<
   namespace?: string;
   ttl?: number;
 }
-
-export type CacheableAsyncFunction2 = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ...args: any[]
-) => Promise<unknown>;
-
-/*
-export const withCache = async <
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TFunction extends (...args: any[]) => Promise<any>,
->(params: {
-  fn: TFunction;
-  key: unknown[];
-}): Promise<Awaited<ReturnType<TFunction>>> => {
-  const { fn } = params;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return await fn();
-};
-*/
-
-// export const withCache = async <TResult, TKey extends unknown[]>(params: {
-export const withCache = async <TResult, TKey extends CacheKeyTuple>(params: {
-  key: TKey;
-  fn: (params: { key: TKey }) => Promise<TResult>;
-}): Promise<{ data: TResult; meta: { cached: boolean } }> => {
-  const { fn, key } = params;
-  return {
-    data: await fn({ key }),
-    meta: {
-      cached: true,
-    },
-  };
-};
-
-export const createAsyncCache = () => {
-  return withCache;
-};
