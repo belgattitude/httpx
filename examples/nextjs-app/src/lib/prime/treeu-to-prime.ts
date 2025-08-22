20ece09749ccb0db77b7b95e643d4c43fd860542
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-08-22 21:00:44 +0200

    feat: example treeu mapping

--- examples/nextjs-app/src/lib/prime/treeu-to-prime.ts
+++ examples/nextjs-app/src/lib/prime/treeu-to-prime.ts
@@ -1,15 +1,17 @@
-import type { TreeNode } from '@httpx/treeu';
+import type { TreeNode, TreeNodeValue } from '@httpx/treeu';
 import type { TreeNode as PrimeTreeNode } from 'primereact/treenode';
 
-export const treeuToPrime = <T>(treeuNodes: TreeNode<T>[]): PrimeTreeNode[] => {
+export const treeuToPrime = <T extends TreeNodeValue | undefined>(
+  treeuNodes: TreeNode<T>[]
+): PrimeTreeNode[] => {
   return treeuNodes.map((node) => {
     const value = node.value as T;
     return {
       key: node.id,
-      label: value,
-      // data: node.value as T,
+      label: (value as unknown as { label: string })?.label ?? undefined,
+      data: value,
       children: node.children ? treeuToPrime<T>(node.children) : undefined,
       // Add other mappings if needed
-    };
+    } satisfies PrimeTreeNode;
   });
 };
