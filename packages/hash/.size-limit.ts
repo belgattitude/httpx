034811de68068fcbeff743971684dbcd19d5a815
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2025-10-09 16:15:39 +0200

    fix(hash): wrong export (#2594)
    * fix: wrong export
    
    * fix: wrong export

--- packages/hash/.size-limit.ts
+++ packages/hash/.size-limit.ts
@@ -2,12 +2,6 @@ import type { SizeLimitConfig } from 'size-limit';
 
 module.exports = [
   {
-    name: 'import { md5 } (ESM)',
-    path: ['dist/index.js'],
-    import: '{ md5 }',
-    limit: '570B',
-  },
-  {
     name: 'import xxhash-wasm (ESM)',
     path: ['dist/xxhash-wasm/index.js'],
     import: '*',
