c903ca9c7857efd7d28908f5836765e70b078ba5
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-10-14 07:33:43 +0200

    feat: use globalThis to preserve wasm instance

--- packages/hash/.size-limit.ts
+++ packages/hash/.size-limit.ts
@@ -5,6 +5,6 @@ module.exports = [
     name: 'import xxhash-wasm (ESM)',
     path: ['dist/xxhash-wasm/index.js'],
     import: '*',
-    limit: '800B',
+    limit: '810B',
   },
 ] satisfies SizeLimitConfig;
