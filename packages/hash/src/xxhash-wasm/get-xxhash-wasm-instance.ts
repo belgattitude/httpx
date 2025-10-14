569529bf72af09d598e65e6642a523fc7715aefe
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-10-14 07:32:05 +0200

    feat: use globalThis to preserve wasm instance

--- packages/hash/src/xxhash-wasm/get-xxhash-wasm-instance.ts
+++ packages/hash/src/xxhash-wasm/get-xxhash-wasm-instance.ts
@@ -3,27 +3,29 @@ import type { XXHashAPI } from 'xxhash-wasm';
 declare global {
   // eslint-disable-next-line no-var
   var xxhashInstance: XXHashAPI | null | undefined;
+  // eslint-disable-next-line no-var
+  var xxhashInstancePromise: Promise<XXHashAPI> | null | undefined;
 }
 
 globalThis.xxhashInstance ??= null;
 
-let initPromise: Promise<XXHashAPI> | null = null;
+globalThis.xxhashInstancePromise ??= null;
 
 export const getXXHashWasmInstance = async (): Promise<XXHashAPI> => {
   if (globalThis.xxhashInstance) {
     return globalThis.xxhashInstance;
   }
-  if (initPromise !== null) {
-    return initPromise;
+  if (globalThis.xxhashInstancePromise) {
+    return globalThis.xxhashInstancePromise;
   }
-  initPromise = import('xxhash-wasm')
+  globalThis.xxhashInstancePromise = import('xxhash-wasm')
     .then((mod) => mod.default())
     .catch((e) => {
-      initPromise = null;
+      globalThis.xxhashInstancePromise = null;
       const msg = `xxhash-wasm module failed to load, did you forget to install it? (${(e as Error).message})`;
       throw new Error(msg);
     });
-  globalThis.xxhashInstance = await initPromise;
-  initPromise = null;
+  globalThis.xxhashInstance = await globalThis.xxhashInstancePromise;
+  globalThis.xxhashInstancePromise = null;
   return globalThis.xxhashInstance;
 };
