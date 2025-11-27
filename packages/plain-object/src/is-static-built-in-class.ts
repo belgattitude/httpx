45bf346b1d20f42ff1807c3b9aa904215c0f96fe
Sébastien Vanvelthem <belgattitude@gmail.com>
2025-11-27 14:47:57 +0100

    feat: deprecate usage of isStaticBuiltinClass

--- packages/plain-object/src/is-static-built-in-class.ts
+++ packages/plain-object/src/is-static-built-in-class.ts
@@ -1,5 +1,8 @@
 export type StaticBuiltInClass = Math | JSON | Atomics;
 
+/**
+ * @deprecated
+ */
 export const isStaticBuiltInClass = (v: unknown): v is StaticBuiltInClass => {
   return v === Math || v === JSON || v === Atomics;
 };
