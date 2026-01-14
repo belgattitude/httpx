830f115c173f395f60bd242747a625115c6b9093
Sébastien Vanvelthem <belgattitude@gmail.com>
2026-01-14 02:27:07 +0100

    chore: simplify vitest configs

--- packages/plain-object/vitest.config.ts
+++ packages/plain-object/vitest.config.ts
@@ -1,11 +1,5 @@
-import codspeedPlugin from '@codspeed/vitest-plugin';
 import { getVitestBaseConfig } from '@httpx/devtools-vitest';
 import { defineConfig } from 'vitest/config';
-
-const testFiles = ['./src/**/*.test.{js,ts}', './test/**/*.test.{js,ts}'];
-
-const isCodeSpeedEnabled = process.env?.CODSPEED === '1';
-const cspeed = isCodeSpeedEnabled ? codspeedPlugin() : undefined;
 
 export default defineConfig(
   getVitestBaseConfig({
